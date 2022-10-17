a6881d035064c2d59650ae463e7e48259d43dddf
miarta <miarta.igede@gmail.com>
2022-10-17 12:06:51 +0800

    market api

--- app/Http/Controllers/api/v1/MarketController.php
+++ app/Http/Controllers/api/v1/MarketController.php
@@ -10,11 +10,23 @@ use Illuminate\Http\Request;
 class MarketController extends Controller
 {
     public function market(){
+        $market = [];
         $data = Market::with('product')->where('status',true)->orderBy('customer','asc')->get();
+        foreach ($data as $key => $value) {
+            $market[] = [
+                'id'=>$value->id,
+                'avatar'=>$value->avatar,
+                'customer'=>$value->customer,
+                'product'=>$value->product->name,
+                'qty'=>$value->qty,
+                'satuan'=>$value->product->satuan,
+                'text'=> "Saya Mau Pesan ".$value->product->name." " . $value->qty. " ".$value->product->satuan." Apakah Kamu Menjualnya?"
+            ];
+        }
         return response()->json([
             'status'    => 200,
             'msg'       => 'Request customer on marker',
-            'data'      => $data,
+            'data'      => $market,
         ],200);
     }
 }
