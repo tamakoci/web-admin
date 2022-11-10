8c5cdb63200f24219228a3f2583c0a154a178b2e
miarta <miarta.igede@gmail.com>
2022-11-10 14:56:05 +0800

    collected cron bugs

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -19,7 +19,11 @@ class CronController extends Controller
         $invest = Investment::with(['userTernak','userTernak.ternak','userTernak.ternak.produk'])->where('status',1)->get();
         // dd($invest);
         foreach ($invest as $key => $value) {
-            $start      = date("Y-m-d H:i:s", strtotime($value->created_at));
+            if($value->userTernak->ternak_id != 4 ){
+                $start      = date("Y-m-d H:i:s", strtotime($value->created_at));
+            }else{
+                $start      = date("Y-m-d H:i:s", strtotime($value->updated_at));
+            }
             $now        = date("Y-m-d H:i:s");
             $end        = date('Y-m-d H:i:s',strtotime("+1 day", strtotime($start)));
 
@@ -34,47 +38,28 @@ class CronController extends Controller
             try {
                 if($value->userTernak->ternak_id != 4){ //cek jika userternak bukan domba. domba_id = 4 ;
                     
-                    if($end > $now){ // cek tanggal berakhir jika lebih besar dari hari ini berarti masih berjalan;
+                    if(($perjam + $remains + $collected) < $total){ //jika fee per jam (pembagian perjam) + remain (pendapatan yg masuk) lebih besar dari total_bisa_dapat
+                        $update = [
+                            'remains' => $remains + $perjam,
+                        ];
                     
-                        if(($perjam + $remains + $collected) < $total){ //jika fee per jam (pembagian perjam) + remain (pendapatan yg masuk) lebih besar dari total_bisa_dapat
-                            $update = [
-                                'remains' => $remains + $perjam,
-                            ];
-                        
-                        }else{
-                           if($remains == 0 && $collected == $total){
-                                $update = [
-                                    'remains' => $remains + $kurang, //berikan sisa belum dapat
-                                    'status'  => 0
-                                ];
-                           }else{
-                                $update = [
-                                    'remains' => $remains + $perjam,
-                                ];
-                           }
-                          
-                        }
-                        Investment::find($value->id)->update($update);
-                    }else{ //jika tanggal berakhir jika lebih kecil dari hari ini, berikan sisa remain dan ubah ke nonaktif
-                         if($remains == 0 && $collected == $total){
-                                $update = [
-                                    'remains' => $remains + $kurang, //berikan sisa belum dapat
-                                    'status'  => 0
-                                ];
-                           }else{
-                                $update = [
-                                    'remains' => $remains + $perjam,
-                                ];
-                           }
-                        
-                        Investment::find($value->id)->update($update);
+                    }else if(($perjam + $remains + $collected) > $total){
+                        $update = [
+                            'remains' => $remains + $kurang,
+                        ];
+                    }else{
+                        $update = [
+                            'remains' => $remains + $perjam,
+                            'status'  => 0
+                        ];
                     }
+                    Investment::find($value->id)->update($update);
                 }else{ // juka ternak domba penghasil daging
                     $tgl_beli   = date("Y-m-d H:i:s", strtotime($value->userTernak->buy_date));
                     $tgl_akhir  = date('Y-m-d H:i:s',strtotime("+7 day", strtotime($tgl_beli)));
                     if($now > $tgl_akhir){ //jika sudah melebihi tgl umur ternak maka bonus daging dikirmkan ke wallet
                         Investment::find($value->id)->update([
-                                'remains' => $remains + $kurang,
+                                'remains' => $kurang,
                                 'status'  => 0
                         ]);
                     }
