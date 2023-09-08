cf5b4fab401ff845842ca1889820cd71e7082f0f
miartayasa <miartayasa10@gmail.com>
2023-09-08 18:27:13 +0800

    wd api

--- app/Http/Controllers/api/v1/WithdrawController.php
+++ app/Http/Controllers/api/v1/WithdrawController.php
@@ -10,9 +10,11 @@ use App\Models\Transaction;
 use App\Models\User;
 use App\Models\UserBank;
 use App\Models\UserWallet;
+use App\Models\Withdrawl;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class WithdrawController extends Controller
@@ -116,81 +118,119 @@ class WithdrawController extends Controller
         return response()->json(['status'=>200,'message'=>'Withdrawal in proceess','data'=>json_decode($res,true)]);
     }
 
+    // public function withdraw(Request $request){
+    //     $validate = Validator::make($request->all(),[
+    //         'diamon'        => 'required|numeric|min:1000',
+    //         'user_bank_id'  => 'required',
+    //     ]);
+    //     if($validate->fails()){
+    //         return response()->json(['status'=>'401','message'=>'Validation Error','errors'=>$validate->getMessageBag()],401);
+    //     }
+    //     $user = Auth::user();
+    //     $wallet = UserWallet::where('user_id',$user->id)->orderByDesc('id')->first();
+    //     $diamon = $wallet->diamon;
+    //     if($request->diamon > $diamon){
+    //         return response()->json(['status'=>'401','message'=>'Not Enough Diamonds',],401);
+    //     }
+    //     $userbanks = UserBank::with('bank')->where('id',$request->user_bank_id)->first();
+    //     $data=[
+    //         'merchantAppCode' => $this->app,
+    //         'merchantAppPassword' => $this->pass,
+    //         'withdrawalNo'  => Transaction::trxID('WD'),
+    //         'accountName'   => $userbanks->account_name,
+    //         'accountNo'     => $userbanks->account_number,
+    //         'bankCode'      => $userbanks->bank->name,
+    //         'bankName'      => $userbanks->bank->code,
+    //         'bankBranch'    => 'Asia',
+    //         'bankCity'      => $userbanks->bank_city,
+    //         'requestAmount' => $request->diamon * 100,
+    //         'additionalMsg' => 'Tamakci Wthdrawl ' .$request->diamon. ' diamon = ' . $request->diamon * 100 .'IDR',
+    //         'processURL'    => url('proccess')
+    //     ];
+    //     $res = $this->send($this->url.'merchant-withdrawal.php',json_encode($data));
+    //     $arr = json_decode($res,true);
+    //     if($arr['success'] == 1){
+    //         Payment::create([
+    //             'user_id'   => $user->id,
+    //             'mark'      => 'WD',
+    //             'order_no'  => $arr['result']['withdrawalNo'],
+    //             'amount'    => $arr['result']['transferAmount'],
+    //             'diamon'    => $request->diamon,
+    //             'fee'       => $arr['result']['feeAmount'],
+    //             'desc'      => 'Withdrawal '.$request->diamon.' Diamon',
+    //             'expired'   => null,
+    //             'checkout_url'=>'-',
+    //             'pay_method'=> 'Transfer Bank '. $arr['result']['bankName'] .' A/N : '. $arr['result']['accountName'],     
+    //             'status'    => 2
+    //         ]);
+    //         UserWallet::create([
+    //                 'user_id'=>$user->id,
+    //                 'diamon'=>$wallet->diamon - $request->diamon,
+    //                 'pakan'=>$wallet->pakan,
+    //                 'hasil_ternak'=>$wallet->hasil_ternak
+    //         ]);
+    //         Transaction::create([
+    //             'user_id' => $user->id,
+    //             'last_amount' => $wallet->diamon,
+    //             'trx_amount' => $request->diamon,
+    //             'final_amount'=> $wallet->diamon - $request->diamon,
+    //             'trx_type'=>'-',
+    //             'detail'=>'Withdrawal Diamon',
+    //             'trx_id' => $arr['result']['withdrawalNo']
+    //         ]);
+    //         return response()->json([
+    //             'status'=> 200,
+    //             'msg'   => 'Withdrawl Success',
+    //             'data'  =>  [
+    //                 'trxID'     => $arr['result']['withdrawalNo'],
+    //                 'amount'    => $arr['result']['transferAmount'],
+    //                 'diamon'    => $request->diamon,
+    //                 'fee'       => $arr['result']['feeAmount'],
+    //                 'desc'      => 'Withdrawal '.$request->diamon.' Diamon',
+    //                 'method'=> 'Transfer Bank '. $arr['result']['bankName'] .' A/N : '. $arr['result']['accountName'],
+    //             ]
+    //         ],200);
+    //     }else{
+    //         return response()->json(['status'=>500,'msg'=>'Transaction Failed','errors'=>$arr],500);
+    //     }     
+    // }
     public function withdraw(Request $request){
-        $validate = Validator::make($request->all(),[
-            'diamon'        => 'required|numeric|min:1000',
-            'user_bank_id'  => 'required',
+         $validate = Validator::make($request->all(),[
+            'amount'        => 'required|numeric|min:1000',
         ]);
         if($validate->fails()){
             return response()->json(['status'=>'401','message'=>'Validation Error','errors'=>$validate->getMessageBag()],401);
         }
-        $user = Auth::user();
-        $wallet = UserWallet::where('user_id',$user->id)->orderByDesc('id')->first();
-        $diamon = $wallet->diamon;
-        if($request->diamon > $diamon){
-            return response()->json(['status'=>'401','message'=>'Not Enough Diamonds',],401);
-        }
-        $userbanks = UserBank::with('bank')->where('id',$request->user_bank_id)->first();
-        $data=[
-            'merchantAppCode' => $this->app,
-            'merchantAppPassword' => $this->pass,
-            'withdrawalNo'  => Transaction::trxID('WD'),
-            'accountName'   => $userbanks->account_name,
-            'accountNo'     => $userbanks->account_number,
-            'bankCode'      => $userbanks->bank->name,
-            'bankName'      => $userbanks->bank->code,
-            'bankBranch'    => 'Asia',
-            'bankCity'      => $userbanks->bank_city,
-            'requestAmount' => $request->diamon * 100,
-            'additionalMsg' => 'Tamakci Wthdrawl ' .$request->diamon. ' diamon = ' . $request->diamon * 100 .'IDR',
-            'processURL'    => url('proccess')
-        ];
-        $res = $this->send($this->url.'merchant-withdrawal.php',json_encode($data));
-        $arr = json_decode($res,true);
-        if($arr['success'] == 1){
-            Payment::create([
+        $wallet = UserWallet::getWallet();
+        if($wallet->diamon - $request->amount <0)return redirect()->back()->with('error','Not Enough Gems');
+        $user = $request->user();
+        // dd($request->all());
+
+        DB::beginTransaction();
+        try {
+            Withdrawl::create([
+                'user_id'       => $user->id,
+                'amount'        => $request->amount,
+                'currency'      => 'IDR',
+                'charge'        => 0,
+                'final_amount'  => $request->amount,
+                'status'        => 1
+            ]);
+          
+            UserWallet::Create([
                 'user_id'   => $user->id,
-                'mark'      => 'WD',
-                'order_no'  => $arr['result']['withdrawalNo'],
-                'amount'    => $arr['result']['transferAmount'],
-                'diamon'    => $request->diamon,
-                'fee'       => $arr['result']['feeAmount'],
-                'desc'      => 'Withdrawal '.$request->diamon.' Diamon',
-                'expired'   => null,
-                'checkout_url'=>'-',
-                'pay_method'=> 'Transfer Bank '. $arr['result']['bankName'] .' A/N : '. $arr['result']['accountName'],     
-                'status'    => 2
+                'diamon'    => $wallet->diamon - $request->amount,
+                'pakan'     => $wallet->pakan,
+                'vaksin'     => $wallet->vaksin,
+                'tools'     => $wallet->tools,
+                'hasil_ternak'     => $wallet->hasil_ternak,
             ]);
-            UserWallet::create([
-                    'user_id'=>$user->id,
-                    'diamon'=>$wallet->diamon - $request->diamon,
-                    'pakan'=>$wallet->pakan,
-                    'hasil_ternak'=>$wallet->hasil_ternak
-            ]);
-            Transaction::create([
-                'user_id' => $user->id,
-                'last_amount' => $wallet->diamon,
-                'trx_amount' => $request->diamon,
-                'final_amount'=> $wallet->diamon - $request->diamon,
-                'trx_type'=>'-',
-                'detail'=>'Withdrawal Diamon',
-                'trx_id' => $arr['result']['withdrawalNo']
-            ]);
-            return response()->json([
-                'status'=> 200,
-                'msg'   => 'Withdrawl Success',
-                'data'  =>  [
-                    'trxID'     => $arr['result']['withdrawalNo'],
-                    'amount'    => $arr['result']['transferAmount'],
-                    'diamon'    => $request->diamon,
-                    'fee'       => $arr['result']['feeAmount'],
-                    'desc'      => 'Withdrawal '.$request->diamon.' Diamon',
-                    'method'=> 'Transfer Bank '. $arr['result']['bankName'] .' A/N : '. $arr['result']['accountName'],
-                ]
-            ],200);
-        }else{
-            return response()->json(['status'=>500,'msg'=>'Transaction Failed','errors'=>$arr],500);
-        }     
+            DB::commit();
+            return redirect()->back()->with('success','Withdrawl Request Send');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return redirect()->back()->with('error','Error: '.$th->getMessage());
+        }
     }
 
     public function send($url,$data){
